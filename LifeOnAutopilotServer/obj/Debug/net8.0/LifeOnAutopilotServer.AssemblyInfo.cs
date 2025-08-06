d523347174cc0bde0180ff5db8a6828d9f64948d
adam.sinkovics <adam.sinkovics@gmail.com>
2025-08-06 21:19:53 +0200

    feat: update gitignore

--- LifeOnAutopilotServer/obj/Debug/net8.0/LifeOnAutopilotServer.AssemblyInfo.cs
+++ LifeOnAutopilotServer/obj/Debug/net8.0/LifeOnAutopilotServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LifeOnAutopilotServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+183e049e2e8d56c46a9ae59f9fc25de7db52e3f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("LifeOnAutopilotServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LifeOnAutopilotServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
